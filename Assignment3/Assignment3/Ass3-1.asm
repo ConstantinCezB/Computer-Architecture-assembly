0974d1eb7d11a7593fc4a296709ccffd4f51afc1
Constantin Cezar Begu <38817297+ConstantinCezB@users.noreply.github.com>
2019-03-17 21:13:50 -0400

    Assignment 3 q 1 is complete

--- Assignment3/Assignment3/Ass3-1.asm
+++ Assignment3/Assignment3/Ass3-1.asm
@@ -1,4 +1,4 @@
-include Irvine32.inc
+include C:\Irvine\Irvine32.inc
 
 .data	
 	message1 BYTE "What do you want to do now? > ", 0
@@ -13,22 +13,31 @@ include Irvine32.inc
 	message9 BYTE "Stack is ", 0
 	message10 BYTE "before ArrayToStack", 0
 	message11 BYTE "after ArrayToStack", 0
-	message12 BYTE "I am exiting… Thank you Honey… and Get lost…", 0
+	message12 BYTE "I am exiting... Thank you Honey... and Get lost...", 0
 	message13 BYTE "What do you want to do now? > ", 0
 	errorMessage1 BYTE "Already loaded", 0
 	errorMessage2 BYTE "Empty stack", 0
+	errorMessage3 BYTE "Load vector first!", 0
+	errorMessage4 BYTE "Number must be bigger then 0!", 0 
 	vector DWORD 100 DUP(0)
 	vectorSize DWORD 0
 	stackToPrint DWORD LENGTHOF vector DUP(0)
 	isStackEmptyBool BYTE 1
-	trace BYTE "WORKS", 0
+	vactorHasValues BYTE 0
+	space BYTE " ", 0
 
 .code
+
+DoSpace PROC USES edx
+	mov edx, OFFSET space
+	call WriteString
+	; call Crlf
+DoSpace ENDP
 	
 main proc
 	
-	MOV edx, OFFSET message13
 	do_while:	
+		MOV edx, OFFSET message13
 		CALL WriteString
 		CALL ReadInt
 		CALL Crlf
@@ -41,25 +50,130 @@ main proc
 		next_check:
 		cmp eax, 1
 		JNE next_check1
-		CALL Option1
+
+	;Option 1 : loads array into stack + ui
+		CMP vactorHasValues, 0 
+		JE error3
+		CMP isStackEmptyBool, 0 
+		JE error1
+
+		CALL Crlf
+		MOV edx, OFFSET message8
+		CALL WriteString
+		CALL PrintVector
+		MOV edx, OFFSET message10
+		CALL WriteString
+		CALL Crlf
+
+		;Logic
+		CALL ArrayToStack
+		CALL CopyInverseToStackToPrint
+		CALL EmptyVector
+		;End Logic
+
+		MOV edx, OFFSET message9
+		CALL WriteString
+		CALL PrintStackToPrint
+		MOV edx, OFFSET message11
+		CALL WriteString
+		CALL Crlf
+
+		MOV edx, OFFSET message8
+		CALL WriteString
+		CALL PrintVector
+		MOV edx, OFFSET message11
+		CALL WriteString
+		CALL Crlf
+		MOV isStackEmptyBool, 0
+		CALL IsStackEmpty
+		CALL Crlf
+		CALL Crlf
+		CALL DoSpace
+		JMP end_if1
+
+		error1:
+		MOV edx, OFFSET errorMessage1
+		CALL WriteString
+		CALL Crlf
+		CALL Crlf
+
+		end_if1:
+	;end option 1
+
 		next_check1:
 		cmp eax, 2
 		JNE next_check2
-		CALL Option2
+
+	;Option 2 : loads stack into array + UI
+		CMP vactorHasValues, 0 
+		JE error3
+		CMP isStackEmptyBool, 1
+		JE error2
+
+
+		CALL Crlf
+		MOV edx, OFFSET message9
+		CALL WriteString
+		CALL PrintStackToPrint
+		MOV edx, OFFSET message10
+		CALL WriteString
+		CALL Crlf
+
+		;Logic
+		CALL StackToArray
+		CALL EmptyStackToPrint
+		;End Logic
+
+		MOV edx, OFFSET message8
+		CALL WriteString
+		CALL PrintVector
+		MOV edx, OFFSET message11
+		CALL WriteString
+		CALL Crlf
+
+		MOV isStackEmptyBool, 1
+		CALL IsStackEmpty
+		CALL Crlf
+		CALL Crlf
+		JMP end_if2
+	
+		error2:
+		MOV edx, OFFSET errorMessage2
+		CALL WriteString
+		CALL Crlf
+		CALL Crlf
+		JMP end_if2
+				
+		error3:
+		MOV edx, OFFSET errorMessage3
+		CALL WriteString
+		CALL Crlf
+		CALL Crlf
+		JMP end_if2
+
+		end_if2:
+	;End option 2
+
 		next_check2:
 
 		JMP do_while
 	end_do:
+		MOV edx, OFFSET message12
+		call WriteString
+		call Crlf
 
 	exit
 main endp
 
+; promts user to enter the vector
 Option0 PROC USES esi eax ebx ecx edx
 	MOV edx, OFFSET message2
 	CALL WriteString
 
-	CALL ReadDec
+	CALL ReadInt
 	MOV vectorSize, eax
+	CMP vectorSize, 1
+	JL errorOption1
 
 	MOV edx, OFFSET message3_1
 	CALL WriteString
@@ -100,100 +214,21 @@ Option0 PROC USES esi eax ebx ecx edx
 	CALL Crlf
 	CALL Crlf
 	MOV isStackEmptyBool, 1
+	MOV vactorHasValues, 1
+	JMP end_option1
+
+	errorOption1:
+	CALL Crlf
+	MOV edx, OFFSET errorMessage4
+	CALL WriteString
+	CALL Crlf
+	CALL Crlf
+
+	end_option1:
 	RET
 Option0 ENDP
 
-Option1 PROC USES esi eax ebx ecx edx
-	POP ESI
-	CMP isStackEmptyBool, 0
-	JE error
-
-	CALL Crlf
-	MOV edx, OFFSET message8
-	CALL WriteString
-	CALL PrintVector
-	MOV edx, OFFSET message10
-	CALL WriteString
-	CALL Crlf
-
-	;Logic
-	CALL ArrayToStack
-	CALL CopyInverseToStackToPrint
-	CALL EmptyVector
-	;End Logic
-
-	MOV edx, OFFSET message9
-	CALL WriteString
-	CALL PrintStackToPrint
-	MOV edx, OFFSET message11
-	CALL WriteString
-	CALL Crlf
-
-	MOV edx, OFFSET message8
-	CALL WriteString
-	CALL PrintVector
-	MOV edx, OFFSET message11
-	CALL WriteString
-	CALL Crlf
-	MOV isStackEmptyBool, 0
-	CALL IsStackEmpty
-	CALL Crlf
-	CALL Crlf
-	JMP end_if
-
-	error:
-	MOV edx, OFFSET errorMessage1
-	CALL WriteString
-	CALL Crlf
-	CALL Crlf
-
-	end_if:
-	PUSH ESI
-	RET
-Option1 ENDP
-
-Option2 PROC USES esi eax ebx ecx edx
-	POP ESI
-	CMP isStackEmptyBool, 1
-	JE error
-
-	CALL Crlf
-	MOV edx, OFFSET message9
-	CALL WriteString
-	CALL PrintStackToPrint
-	MOV edx, OFFSET message10
-	CALL WriteString
-	CALL Crlf
-
-	;Logic
-	CALL StackToArray
-	CALL EmptyStackToPrint
-	;End Logic
-
-	MOV edx, OFFSET message8
-	CALL WriteString
-	CALL PrintVector
-	MOV edx, OFFSET message11
-	CALL WriteString
-	CALL Crlf
-
-	MOV isStackEmptyBool, 1
-	CALL IsStackEmpty
-	CALL Crlf
-	CALL Crlf
-	JMP end_if
-	
-	error:
-	MOV edx, OFFSET errorMessage2
-	CALL WriteString
-	CALL Crlf
-	CALL Crlf
-
-	end_if:
-	PUSH ESI
-	RET
-Option2 ENDP
-
+; displays if the stack is empty or not
 IsStackEmpty PROC 
 	CMP isStackEmptyBool, 1
 	JE is_empty
@@ -207,6 +242,7 @@ IsStackEmpty PROC
 	RET
 IsStackEmpty ENDP
 
+;prints the vector
 PrintVector PROC 
 	MOV ecx, 0
 	MOV ebx, 0
@@ -226,6 +262,7 @@ PrintVector PROC
 	RET
 PrintVector ENDP
 
+; prints the emulated stack (only used for siplay and not storage)
 PrintStackToPrint PROC 
 	MOV ecx, 0
 	MOV ebx, 0
@@ -245,6 +282,7 @@ PrintStackToPrint PROC
 	RET
 PrintStackToPrint ENDP
 
+;replaces all values with 0
 EmptyVector PROC
 	MOV ecx, 0
 	MOV ebx, 0
@@ -261,35 +299,43 @@ EmptyVector PROC
 	RET
 EmptyVector ENDP
 
+; movest the array values to a stack
 ArrayToStack PROC
-	POP ebx
+	POP edx
 	MOV ecx, 0
-	MOV edx, 0
+	MOV edi, 0
+	MOV EBX,vectorSize
+	MOV ESI, OFFSET vector
 
 	do_while_PUSH:	
-		CMP edx, vectorSize
+		CMP edi, EBX
 		JE end_do_PUSH
 
-		MOV eax, [vector + ecx]
+		MOV eax, [ESI + ecx]
 		PUSH eax
 
 		ADD ecx, TYPE vector
-		INC edx
+		INC edi
 		JMP do_while_PUSH
 	end_do_PUSH:
-	PUSH ebx
+	PUSH edx
 	RET
 ArrayToStack ENDP
 
+;moves the stack values to a array
 StackToArray proc
-	POP	ebx
-	MOV eax, vectorSize 
+	POP	edi
+	MOV EBX, vectorSize
+	MOV eax, EBX 
 	MOV edx, TYPE vector
 	MUL edx
 	SUB eax, TYPE vector
+	POP ecx 
+	POP ecx
+	MOV ESI, OFFSET vector
 	do_while_POP:	
 		POP ecx
-		MOV [vector + eax], ecx
+		MOV [ESI + eax], ecx
 
 		CMP eax, 0
 		JE end_do_POP
@@ -297,10 +343,11 @@ StackToArray proc
 		SUB eax, TYPE vector
 		JMP do_while_POP
 	end_do_POP:
-	PUSH ebx
+	PUSH edi
 	RET
 StackToArray ENDP
 
+;used to reverse the fake stack so that it can emulate the real one
 CopyInverseToStackToPrint proc
 	POP ebx
 
@@ -335,6 +382,7 @@ CopyInverseToStackToPrint proc
 	RET
 CopyInverseToStackToPrint ENDP
 
+;emptis the fake stack
 EmptyStackToPrint PROC
 	MOV ecx, 0
 	MOV ebx, 0
